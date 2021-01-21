6f9e52e617d2641beb2982d08feb68d7f115907c
BlaineBeltran <55524257+ComputerEng21@users.noreply.github.com>
2021-01-20 20:39:09 -0600

    Added Linked list

--- Part 1 (Basics)/Chapter 1: Getting Started/test.py
+++ Part 1 (Basics)/Chapter 1: Getting Started/test.py
@@ -1,1 +1,16 @@
-print("Hello World of Python")
+print("Hello LinkedList")
+
+class Node:
+    def __init__(self, val = None, next = None):
+        self.val = val
+        self.next = next
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+n1 = Node(2)
+n2 = Node(3)
+
+ll = Linkedlist()
+n1 = ll.head
